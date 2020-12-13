e19770154fcc79613d8ec6b84a834fd781a368d5
Robert Bjarnason <robert.bjarnason@gmail.com>
2020-12-13 17:19:52 +0000

    Fix

--- processScripts/importToES.sh
+++ processScripts/importToES.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar target/ackeywordscanner-1.0-jar-with-dependencies.jar importToES "/home/robert/data/CommonCrawl/$1/$2/wet.paths.importList" "/home/robert/data/CommonCrawl/$1/$2/hostranks.txt.gz.processed.gz"
+java -jar target/ackeywordscanner-1.0-jar-with-dependencies.jar importToES "/home/robert/data/CommonCrawl/$1/$2/wet.paths.importList" "exampleKeywords/en_v1.csv" "/home/robert/data/CommonCrawl/$1/$2/hostranks.txt.gz.processed.gz"
