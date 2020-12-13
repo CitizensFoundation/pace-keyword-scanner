486cd5a9394e6e7bfbbc2a8ed4cee0a1e83331ec
Robert Bjarnason <robert.bjarnason@gmail.com>
2020-12-13 21:08:50 +0000

    Work on scan

--- src/main/java/org/cf/acks/Main.java
+++ src/main/java/org/cf/acks/Main.java
@@ -162,7 +162,7 @@ public class Main {
         logger.info("CPU cores available: {}", Runtime.getRuntime().availableProcessors());
 
         final int poolSize = Runtime.getRuntime().availableProcessors();
-        final int maxScheduled = poolSize * 7;
+        final int maxScheduled = poolSize * 5;
 
         logger.info("Allocating a thread pool of size {}.", poolSize);
 
