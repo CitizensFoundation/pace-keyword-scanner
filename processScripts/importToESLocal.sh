f7f00953429f4b3be95007c927612dc34bd6e69c
Robert Bjarnason <robert.bjarnason@gmail.com>
2021-01-22 23:18:48 +0000

    Work on search

--- processScripts/importToESLocal.sh
+++ processScripts/importToESLocal.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar target/ackeywordscanner-1.0-jar-with-dependencies.jar importToES "/home/robert/data/CommonCrawl/$1/$2/wet.paths.importList" 127.0.0.1 "/home/robert/data/CommonCrawl/$1/$2/hostranks.txt.gz.processed.gz"
+java -jar target/ackeywordscanner-1.0-jar-with-dependencies.jar importToES "/home/robert/data/CommonCrawl/2019/04/files3.txt" "exampleKeywords/en_v1.csv" "/home/robert/data/CommonCrawl/2019/04/hostranks.txt.gz.processed.gz"
