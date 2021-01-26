e8d2fe96d4059b133ae2fea3dd14f5e34734faf6
Robert Bjarnason <robert.bjarnason@gmail.com>
2021-01-26 20:02:40 +0000

    Work on scan

--- processScripts/scan.sh
+++ processScripts/scan.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 importPath="/home/robert/data/CommonCrawl/$1/$2/wet.paths.importList"
 echo $importPath
-java -Xmx100g -jar target/ackeywordscanner-1.0-jar-with-dependencies.jar scan "$importPath" exampleKeywords/en_v1.csv
+java -Xmx100g -jar -Djna.library.path=/usr/local/lib target/ackeywordscanner-1.0-jar-with-dependencies.jar scan "$importPath" exampleKeywords/en_v1.csv
