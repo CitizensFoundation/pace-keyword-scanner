d81471165e9b5c3f0ac17ac6991b5dc1ebf0ee54
Robert Bjarnason <robert.bjarnason@gmail.com>
2021-02-20 19:33:22 +0000

    From dev

--- training/model_training.py
+++ training/model_training.py
@@ -17,6 +17,7 @@ class ModelTraining:
         manager.setup_all_from_xls()
         trainingData = manager.get_training_data({})
         #trainingData = manager.get_training_data({"topic": "Left behind", "subTopic": "Economics"})
+        #trainingData = manager.get_training_data({"topic": "Left behind"})
 
         def random_seed():
             return 0.5
@@ -25,7 +26,7 @@ class ModelTraining:
 
         random.shuffle(trainingData, random_seed)
 
-        splitIndex = int(length*0.9)
+        splitIndex = int(length*0.8)
 
         # Preparing train data
         train_data = trainingData[:splitIndex]
