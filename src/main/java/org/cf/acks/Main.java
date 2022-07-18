6c1473a87572bef84c252a73bf3093625f87d6f6
Robert Bjarnason <robert.bjarnason@gmail.com>
2022-07-18 15:57:54 +0000

    Work

--- src/main/java/org/cf/acks/Main.java
+++ src/main/java/org/cf/acks/Main.java
@@ -41,7 +41,7 @@ public class Main {
 
     // PRODUCTION
     //private static final String esHostname="search-demos-v1-hujolmcnrbsjb3fh3pfhbvtsbu.us-east-1.es.amazonaws.com";
-    private static final String esHostname="https://search-popai-nlixyfy37n3b2zipuqzul6hklm.us-east-1.es.amazonaws.com/";
+    private static final String esHostname="https://search-popai-nlixyfy37n3b2zipuqzul6hklm.us-east-1.es.amazonaws.com";
 
     private static final Integer esPort=443;
     private static final String esProtocol="https";
